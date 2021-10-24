6ce838f16e00870a0050e9ecc64a6dbba43582b4
droflim <estay.milford@gmail.com>
2021-10-24 18:53:40 -0300

    Test Unitario Footer.vur
    Test Unitario Footer.vue

--- amigurumi-store/tests/e2e/specs/test.js
+++ amigurumi-store/tests/e2e/specs/test.js
@@ -2,7 +2,8 @@
 
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Welcome to Your Vue.js App')
+    cy.visit('/home')
+    cy.visit('/productos')
+    
   })
 })
